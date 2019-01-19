152b2a9579001a85b41bf9769175f80063265911
GarretR <garret.rueckert.guitar@gmail.com>
2019-01-18 19:47:34 -0700

    All done, readme nearly complete

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # liri-node-app
-Language Interpretation and Recognition Interface
+## Language Interpretation and Recognition Interface
+##### Author: Garret Rueckert
+***
+
+## About
+LIRI is a command-line Node application that "recognizes" at least 4 commands from users. Liri is connected to the following APIs:
+
+  - ***Spotify*** - track-search, Node specific version of API
+  - ***BandsInTown*** - concert search by artist
+  - ***OMBD*** - movie database search by title
+
+## How to use LIRI
+To use LIRI, simply download the application, set up an .env file with your api keys, and run in the command line with Node. The first argument to LIRI is the command, any following arguments are the search to the relevant APIs. So the console should look something like this:
+```
+$ node liri.js movie-this The Matrix 
+```
+
+#### Valid Commands For LIRI:
+  - ***movie-this***
+  - ***spotify-this-song***
+  - ***concert-this***
+  - ***do-what-it-says***
+
+***movie-this*** - will search the OMBD API for a title based on your search and return the movie's 
+- Title
+- Release year
+- IMDB and Rotten Tomatoes Rating
+- Country (or countries) of production
+- Language
+- Plot
+
+***spotify-this-song*** will search the Spotify API for a track based on your search and return 
+- Song title
+- Album 
+- Artist
+- Preview URL *(if available)*
+- *Search can be refined by adding more info like artist name + the track name*
+
+***concert-this*** will search the BandsInTown API for a concert based on your search (by artist) and return the concert's
+- Venue
+- Location
+- Date
+
+***do-what-it-says*** will input a command to LIRI by reading the file **random.txt**, separating the command from the search by a comma
+**Eg.**
+```
+spotify-this-song,"I Want it That Way"
+```
+
+LIRI automatically formats any searches passed as command-line arguments (beyond the initial command input) 
+
+## Tech Used
+
+LIRI uses a variety of node packages to work:
+
+* [Axios](https://www.npmjs.com/package/axios) - Promise based HTTP client for the browser and node.js
+* [Moment](https://www.npmjs.com/package/moment) - A lightweight JavaScript date library for parsing, validating, manipulating, and formatting dates.
+* [FS](https://nodejs.org/api/fs.html) - Node's built-in file system I/O
+* node.js - Runtime environment for the JavaScript
+* [dotenv](https://www.npmjs.com/package/dotenv) - Module that loads environment variables from a .env file into process.env
+* [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) - node version of hitting the Spotify REST API
+* Git - version control and using .gitignore to hide Spotify keys
+
+
+## Examples
+![](screenshots/spotify-cap1.PNG)
+
+
+
+## Todos
+- Add a log.txt file and write every command and output to it
+- Better format concerts showing and allow users to refine search
+- More testing for break-case scenarios
--- liri.js
+++ liri.js
@@ -1,7 +1,18 @@
+/**
+ * LIRI Node Command Line App
+ * 
+ * @author Garret Rueckert
+ * 
+ * UofU Bootcamp January 2019
+ */
+
+
+//Global Variables/ Node dependencies
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
+var moment = require("moment");
 var fs = require("fs");
 
 var spotify = new Spotify({
@@ -20,17 +31,15 @@ if (process.argv[3]) {
     }
 }
 
-/*
- * `"https://rest.bandsintown.com/artists/"
- * + artist
- * + "/events?app_id=codingbootcamp"`
- *
- *
- *
- *
- *
+/**
+ * Calls one of the search functions based on what user passes
+ * to it in the command line. Process.argv[2] will be used then 
+ * all arguments past that will format to the search.
+ * 
+ * @param command Command to pass LIRI; possible commands are:
+ * 'movie-this' 'concert-this' 'spotify-this-song' and 
+ * 'do-what-it-says'
  */
-
 function liriCommands(command) {
   switch (command) {
     case "movie-this":
@@ -43,13 +52,21 @@ function liriCommands(command) {
       songSearch(searchTerm);
       break;
     case "do-what-it-says":
+      randomAct();
       break;
-
     default:
+      console.log("I know 'movie-this', 'concert-this'," +
+      " 'spotify-this-song', or 'do-what-it-says' for commands.");
       break;
   }
 }
 
+/**
+ * Hits the OMBD API to print relevant movie info to the 
+ * console based on the search passed to it
+ * 
+ * @param search Movie title to search for
+ */
 function omdbSearch(search) {
   var movieName = "";
   if (search !== "") {
@@ -57,20 +74,23 @@ function omdbSearch(search) {
   } else {
     movieName = "Mr.+Nobody";
   }
-  // Then run a request with axios to the OMDB API with the movie specified
   var queryUrl =
     "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-  // This line is just to help us debug against the actual URL.
-  console.log(queryUrl);
 
   // Then create a request with axios to the queryUrl
   axios
     .get(queryUrl)
     .then(function(response) {
       // If the request with axios is successful
-      // ...
-      console.log(response.data);
+      console.log("\nTitle: "+response.data.Title);
+      console.log("Year: "+response.data.Year);
+      console.log("IMDB Rating: "+response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating: "+response.data.Ratings[1].Value);
+      console.log("Country: "+response.data.Country);
+      console.log("Language:c"+response.data.Language);
+      console.log("Plot:\n"+response.data.Plot);
+      console.log("Actors: "+response.data.Actors);
+      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     })
     .catch(function(err) {
       if (err.response) {
@@ -86,6 +106,12 @@ function omdbSearch(search) {
     });
 }
 
+/**
+ * Hits the BandsInTown API and prints concert info
+ * for whatever artist/band searched for to the console
+ * 
+ * @param search Band to search for upcoming concerts for
+ */
 function bandSearch(search) {
   var bandName = "";
   if (search !== "") {
@@ -103,8 +129,11 @@ function bandSearch(search) {
     .get(queryUrl)
     .then(function(response) {
       // If the request with axios is successful
-      // ...
-      console.log(response.data);
+      console.log("\nVenue: " + response.data[0].venue.name);
+      console.log("Location: " + response.data[0].venue.city 
+        + ", " + response.data[0].venue.country);
+      console.log(moment(response.data[0].datetime).format("M/D/YYYY"));
+      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     })
     .catch(function(err) {
       if (err.response) {
@@ -120,12 +149,18 @@ function bandSearch(search) {
     });
 }
 
+/**
+ * Hits the Spotify API with the @search passed to it 
+ * and prints the song info to the console
+ * 
+ * @param search Song to search for
+ */
 function songSearch(search) {
   var songName = "";
   if (search !== "") {
     songName = search;
   } else {
-    songName = "Stairway+to+Heaven";
+    songName = "The+Sign+Ace+of+Base";
   }
 
   spotify.search({ type: "track", query: songName }, function(
@@ -139,12 +174,25 @@ function songSearch(search) {
     console.log("Album: " + data.tracks.items[0].album.name);
     console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
     console.log("Preview URL: " + data.tracks.items[0].preview_url);
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
   });
 }
 
+/**
+ * Reads random.txt and does whatever LIRI command is 
+ * written there (formatted by a comma)
+ */
 function randomAct(){
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
 
+    console.log(data);
+    var dataArr = data.split(',');
+    searchTerm = dataArr[1];
+    liriCommands(dataArr[0]);
+  });
 }
 
 
